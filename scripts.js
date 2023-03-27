1fbb9d59bd80bec7eafefdfd2718bafc2d441088
Bdong1275 <93674827+Bdong1275@users.noreply.github.com>
2023-03-26 22:15:48 -0500

    Update scripts.js
    finalized widget for testing

--- scripts.js
+++ scripts.js
@@ -1,22 +1,7 @@
 
-//connect btn to options - DONE
-//need to connect this to collections api
-//need to figure a way to delete image widgets and create new ones for filter result
-//or replace images in all image widget with filter result
-
 document.getElementById("Filterbtn").onclick = function () {
-    
-    // dmAPI.loadCollectionsAPI()
-    //     .then(function(colAPI) {
-    //         //let collection - variable
-    //         //collection.price
-    //         let collection = colAPI.data("All properties").where("Price", "EQ", "$690, 000").get();
-    //         let newC = collection.where("id_test", "LT", "10").get();
-    //     })
-    //     .then(function(values) {
-    //         console.log(values)
-    //     })
-    
+   
+    //the available options for user to select
     let status, minPrice, maxPrice, minSize, maxSize, bath, garage, community, floorPlan;
     status    = document.getElementById("status-select");
     minPrice  = document.getElementById("minPrice-select");
@@ -28,31 +13,58 @@ document.getElementById("Filterbtn").onclick = function () {
     garages   = document.getElementById("garages-select");
     community = document.getElementById("community-select");
     floorPlan = document.getElementById("floorPlan-select");
-    //let test = document.getElementById("floor_plan-select");
-    let x = Number(beds.value);
-    console.log(x);
-    //let text = test.options[test.selectedIndex].text;
-    let statusText, minText, maxText, minSizeText, maxSizeText, bedText, bathText, garageText, commText, planText;
-    statusText    = status.options[status.selectedIndex].text;
-    minPriceText  = minPrice.options[minPrice.selectedIndex].text;
-    maxPriceText  = maxPrice.options[maxPrice.selectedIndex].text;
-    minSizeText   = minSize.options[minSize.selectedIndex].text;
-    maxSizeText   = maxSize.options[maxSize.selectedIndex].text;
-    bedText       = beds.options[beds.selectedIndex].text;
-    bathText      = baths.options[baths.selectedIndex].text;
-    garageText    = garages.options[garages.selectedIndex].text;
-    commText      = community.options[community.selectedIndex].text;
-    floorPlanText = floorPlan.options[floorPlan.selectedIndex].text;
     
+    //<option value>Text</option>
+    let minPriceValue = Number(minPrice.value);
+    let maxPriceValue = Number(maxPrice.value);
+    let minSizeValue = Number(minSize.value);
+    let maxSizeValue = Number(maxSize.value);
+    let bedValue = Number(beds.value);
+    let bathValue = Number(baths.value);
+    let garageValue = Number(garages.value);
 
-    let collection = dmAPI.loadCollectionsAPI().then(api => {
-    return api.data("All properties")
-        .where("Bedrooms", "GT", x)
-        .where("id_test", "LT", 5)
+    let statusValue = Number(status.value);
+    let commValue = Number(community.value);
+    let floorPlanValue = Number(floorPlan.value);
+    
+    let statusText    = status.options[status.selectedIndex].text;
+    let commText      = community.options[community.selectedIndex].text;
+    let floorPlanText = floorPlan.options[floorPlan.selectedIndex].text;
+    
+    //number-really big and really small - in case either end (min/max) wasn't selected
+    dmAPI.loadCollectionsAPI().then(api => {
+        api.data("All properties")
+        //initial comparision of available options
+        //if we compare using status here and status is -1 then nothing will return
+        .where("Bedrooms", "GTE", bedValue)
+        .where("Bathrooms", "GTE", bathValue)
+        .where("Garages", "GTE", garageValue)
+        .where("Price", "BTWN", [minPriceValue, maxPriceValue])
+        .where("Size", "BTWN", [minSizeValue, maxSizeValue])
+        .select("background_image", "address", "Status", "Price", "Size", "Bedrooms", "Bathrooms", "Garages", "Community", "floorPlan")
         .get()
-    }).then(function(values) {
-        console.log(values);
-    })
-    console.log("here:", collection);
-    console.log("hi " + statusText);
+        .then(json => {
+            var img_list = document.getElementById("img_list");
+            for (let object of json.values) {
+                //filter the results using remaining available options
+                //any value other than -1 means that user selected something
+                //the image isn't valid if its text differs from user selected value
+                let valid = true;
+                if (statusValue != -1 && object.data.status != statusText) {
+                    valid = false;
+                }
+                if (commValue != -1 && object.data.community != commText) {
+                    valid = false;
+                }
+                if (floorPlanValue != -1 && object.data.floor_plan == floorPlanText) {
+                    valid = false;
+                }
+                if (valid) {
+                    let x = object.data.background_image;
+                    img_list.innerHTML += "<img src=\"" + x + "\" alt=\"\" width=\"200\" height=\"200\">";
+                }
+            }
+        })
+    })  
+    
 }
